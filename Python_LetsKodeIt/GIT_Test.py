a3105f7d206b075246939497f76b359306c852e1
Rajesh Kumar <rajeshvelur@gmail.com>
2019-06-20 08:38:25 -0600

    Test Git Push

---
+++ Python_LetsKodeIt/GIT_Test.py
@@ -0,0 +1,10 @@
+class Car(object):
+
+    def __init__(self):
+        print("this is car class")
+    def start(self):
+        print("this is Car - Start method")
+    def stop(self):
+        print("this is the car - stop")
+
+x = Car()
